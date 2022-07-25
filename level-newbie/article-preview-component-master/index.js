7db53cf68a980a0a07d81615f0a841b3a5171925
Filip Apatovic <filipapatovic992@gmail.com>
2022-07-25 15:19:38 +0200

    js mobile share funcionality

--- level-newbie/article-preview-component-master/index.js
+++ level-newbie/article-preview-component-master/index.js
@@ -1,1 +1,7 @@
 const shareBtn = document.getElementById('share-btn');
+const shareSection = document.getElementById('card-share');
+
+shareBtn.addEventListener('click',() => {
+    shareSection.classList.toggle('show');
+    shareBtn.classList.toggle("change-color");
+});
