49c12033cc35e8817e6a1509f069be15145b675f
Filip Apatovic <filipapatovic992@gmail.com>
2022-08-12 13:22:33 +0200

    form validation

--- level-newbie/intro-component-with-signup-form-master/index.js
+++ level-newbie/intro-component-with-signup-form-master/index.js
@@ -1,27 +1,41 @@
-const submitBtn = document.getElementById('submit-btn');
+const form = document.getElementById('form');
+const firstName = document.getElementById('first-name');
+const lastName = document.getElementById('last-name');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
 
 
 
-
-submitBtn.addEventListener('click', (e) => {
+form.addEventListener('submit', (e) => {
     e.preventDefault();
-    const emailValue = emailField.value;
-    const mailFormat =/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 
-    if(emailValue === ''){
-        errorMsg.innerHTML = 'Field cannot be empty';
-        errorIcon.classList.add('active');
-        emailField.classList.add('border-active');
+    function isRequired(value) {
+        return value === "" ? false : true;
     }
 
-    if(emailField.value.match(mailFormat)) {
-        errorMsg.innerHTML = '';
-        errorIcon.classList.remove('active');
-        emailField.classList.remove('border-active');
+    function isBetween(length, min, max) {
+        return length < min || length > max ? false : true;
+    } 
 
-    } else {
-        errorMsg.innerHTML = 'Please provide a valid email';
-        errorIcon.classList.add('active');
-        emailField.classList.add('border-active');
-    }
+    function isEmailValid(email) {
+        const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return re.test(email);
+    };
+
+    function isPasswordSecure(password) {
+        const re = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})");
+        return re.test(password);
+    };
+
+    function showError(input, message) {
+        // get the form-field element
+        const formField = input.parentElement;
+        // add the error class
+        formField.classList.remove('success');
+        formField.classList.add('error');
+    
+        // show the error message
+        const error = formField.querySelector('small');
+        error.textContent = message;
+    };
 })
