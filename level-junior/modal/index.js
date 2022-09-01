0c0e3aace9204248bff40367e82ff13edc9bc6e2
Filip Apatovic <filipapatovic992@gmail.com>
2022-09-01 13:18:43 +0200

    modal

---
+++ level-junior/modal/index.js
@@ -0,0 +1,4 @@
+const targets = document.getElementsByClassName('modal-btn');
+const modal = document.getElementById('post-modal');
+const modalTitle = document.getElementById('post-modal-title')
+const modalContent = document.getElementById('post-modal-close')
