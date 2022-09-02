9e64231d266c227deac687a2344cd84e3d199348
Filip Apatovic <filipapatovic992@gmail.com>
2022-09-02 11:27:40 +0200

    inital commit and html fix

--- level-junior/modal/index.js
+++
@@ -1,4 +0,0 @@
-const targets = document.getElementsByClassName('modal-btn');
-const modal = document.getElementById('post-modal');
-const modalTitle = document.getElementById('post-modal-title')
-const modalContent = document.getElementById('post-modal-close')
