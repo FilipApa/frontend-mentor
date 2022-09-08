f8f891c7f4d62cae475b87d2647db7002fe64c0e
Filip Apatovic <filipapatovic992@gmail.com>
2022-09-08 14:32:53 +0200

    add credit card number

--- level-junior/interactive-card-details-form-main/index.js
+++ level-junior/interactive-card-details-form-main/index.js
@@ -1,12 +1,10 @@
 const catdDigits = document.getElementById('card-digits');
 const cardNumFormField = document.getElementById('card-num');
-cardNumFormField.addEventListener('input', (e) => {
-    
-    let target = e.target, 
-    position = target.selectionEnd, 
-    length = target.value.length;
- 
+
+cardNumFormField.addEventListener('input', (e) => {    
+    let target = e.target
+
     target.value = target.value.replace(/[^\dA-Z]/g, '').replace(/(.{4})/g, '$1 ').trim();
-    target.selectionEnd = position += ((target.value.charAt(position - 1) === ' ' && length !== target.value.length) ? 1 : 0);
     catdDigits.innerText = target.value;
+
 });
