8792a53d61614d251f524ccd91aab1ce8cd2d574
Filip Apatovic <filipapatovic992@gmail.com>
2022-09-15 14:06:36 +0200

    calc height in px of bars for chart, set background color add to data

--- level-junior/expenses-chart-component-main/index.js
+++ level-junior/expenses-chart-component-main/index.js
@@ -12,8 +12,22 @@ async function getData() {
     }
 
     const maxBalanceAmount = Math.max(...balance);
+    const chartBarMaxHeight = 150;
 
-    console.log(maxBalanceAmount);
+    for(let value in dailyBalanceRes[0]) {
+        let precentOfMaxVal = Math.ceil( dailyBalanceRes[0][value].amount / maxBalanceAmount * 100 );
+        let chartBarHeight = Math.ceil(precentOfMaxVal / 100 * chartBarMaxHeight);
+        
+        dailyBalanceRes[0][value].barHeight = chartBarHeight;
+
+        if( chartBarHeight === chartBarMaxHeight) {
+            dailyBalanceRes[0][value].backColor = 'hsl(186, 34%, 60%)';
+        } else {
+            dailyBalanceRes[0][value].backColor = 'hsl(10, 79%, 65%)';
+        }
+    }
+
+    console.log(dailyBalanceRes);
 }
 
 getData();
