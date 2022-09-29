ec0016f1b020849b8b58ca126333799bae60bffb
Filip Apatovic <filipapatovic992@gmail.com>
2022-09-29 13:27:14 +0200

    display data

--- level-junior/time-tracking-dashboard-main/index.js
+++ level-junior/time-tracking-dashboard-main/index.js
@@ -1,12 +1,35 @@
 //colors
 const mainSection = document.getElementById('main');
+const dailyBtn = document.getElementById('tab-daily');
+const weeklyBtn = document.getElementById('tab-weekly');
+const mounthlyBtn = document.getElementById('tab-mouthly');
 
-const lightRedWorks = 'hsl(15, 100%, 70%)';
-const softBluePlay =  'hsl(195, 74%, 62%)';
-const lightRedStudy = 'hsl(348, 100%, 68%)';
-const limeGreenExercises = 'hsl(145, 58%, 55%)';
-const violetSocial = 'hsl(264, 64%, 52%)';
-const softOrangeSelfCare = 'hsl(43, 84%, 65%)';
+const timeTrackingTop = [
+    {
+        "color": 'hsl(15, 100%, 70%)',
+        "iconUrl": './images/icon-work.svg'
+    },
+    {
+        "color": 'hsl(195, 74%, 62%)',
+        "iconUrl": './images/icon-play.svg'
+    },
+    {
+        "color": 'hsl(348, 100%, 68%)',
+        "iconUrl": './images/icon-study.svg'
+    },
+    {
+        "color": 'hsl(145, 58%, 55%)',
+        "iconUrl": './images/icon-exercise.svg'
+    },
+    {
+        "color": 'hsl(15, 100%, 70%)',
+        "iconUrl": './images/icon-social.svg'
+    },
+    {
+        "color": 'hsl(43, 84%, 65%)',
+        "iconUrl": './images/icon-self-care.svg'
+    }
+]
 
 const day = [];
 const week = [];
@@ -18,6 +41,37 @@ async function fetchData() {
     return data;
 }
 
+function makeTrackingHtml(data, time) {
+    mainSection.innerHTML = ''
+
+    for (let key of data) {
+        console.log(key)
+        let html = `
+        <section class="time-tracking">
+            <div class="time-tracking-top" style="background-color:${key.timeTrackingTop.color}">
+                <img src="${key.timeTrackingTop.iconUrl}" alt="${key.title} icon">
+            </div>
+            
+            <div class="time-tracking-bottom">
+                <div class="time-tracking-title"> 
+                    <h2>${key.title}</h2>
+                    <img src="./images/icon-ellipsis.svg" alt="Ellipsis icon">
+                </div>
+
+                <div class="time-tracking-hours">
+                    <span class="current-hours">${key.current}hrs</span>
+                    <span class="previus-hours">Last ${time} - ${key.previous}</span>
+
+                </div>
+            </div>
+        </section>
+    `
+    mainSection.insertAdjacentHTML('beforeend', html);
+    }
+   
+
+}
+
 document.addEventListener("DOMContentLoaded", () => {
     fetchData()
     .then(data => {
@@ -27,23 +81,40 @@ document.addEventListener("DOMContentLoaded", () => {
             day[count].title = data[key].title;
             day[count].current = data[key].timeframes.daily.current;
             day[count].previous = data[key].timeframes.daily.previous;
+            day[count].timeTrackingTop = timeTrackingTop[count]
 
             week[count] = {};
             week[count].title = data[key].title;
-            week[count].current = data[key].timeframes.daily.current;
-            week[count].previous = data[key].timeframes.daily.previous;
+            week[count].current = data[key].timeframes.weekly.current;
+            week[count].previous = data[key].timeframes.weekly.previous;
+            week[count].timeTrackingTop = timeTrackingTop[count]
+
 
             mounth[count] = {};
             mounth[count].title = data[key].title;
-            mounth[count].current = data[key].timeframes.daily.current;
-            mounth[count].previous = data[key].timeframes.daily.previous;
+            mounth[count].current = data[key].timeframes.monthly.current;
+            mounth[count].previous = data[key].timeframes.monthly.previous;
+            mounth[count].timeTrackingTop = timeTrackingTop[count];
+
             count++;
         }
+    }).then(() => {
+        makeTrackingHtml(day, "Day");
+        dailyBtn.addEventListener('click', () => {
+            makeTrackingHtml(day, "Day");
+        });
+
+        weeklyBtn.addEventListener('click', () => {
+            makeTrackingHtml(week, "Week");
+        });
+
+        mounthlyBtn.addEventListener('click', () => {
+            makeTrackingHtml(mounth, "Mounth");
+        });
     }).catch(error => {
         console.log(error)
     })
 });
 
-console.log(day)
-console.log(week)
+
 console.log(mounth)
