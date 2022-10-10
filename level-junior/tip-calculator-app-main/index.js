c01c7e48472db90fed4fae6c07b9dbdcb8f16516
Filip Apatovic <filipapatovic992@gmail.com>
2022-10-10 14:55:49 +0200

    js

--- level-junior/tip-calculator-app-main/index.js
+++ level-junior/tip-calculator-app-main/index.js
@@ -2,11 +2,21 @@ function isEmpty(input) {
     return input === "" ? false : true;
 }
 
-function showSuccess(element, msg) {
+function showSuccess( element, msg) {
     const parent = element.parentElement;
     const elementId = element.id;
     const msgEl = document.querySelector(`${elementId} + span`);
 
    msgEl.innerText = msg;
    parent.classList.add('success');
+}
+
+function showError( element, msg) {
+    const parent = element.parentElement;
+    const msgEl = parent.querySelector(`span`);
+
+    parent.classList.remove('success');
+    parent.classList.add('error');
+
+   msgEl.innerText = msg;
 }
