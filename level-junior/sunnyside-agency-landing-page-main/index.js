4ed45ba68dc03ab3be32243bbe48117328fe17ce
Filip Apatovic <filipapatovic992@gmail.com>
2022-10-20 10:55:55 +0200

    js module for navigation

--- level-junior/sunnyside-agency-landing-page-main/index.js
+++ level-junior/sunnyside-agency-landing-page-main/index.js
@@ -1,4 +1,23 @@
-import MobileNav from "../../modules/HamburgerNavigation"
+class MobileNav {
+    constructor(btnSelect, navSelect, cssClass) {
+        this.btn = btnSelect;
+        this.nav = navSelect;
+        this.classToggle = cssClass;
+        this.events();
+    }
+    
 
-const mobileNav = new MobileNav('document.getElementById(hamburger-btn)', 'document.getElementById(header-nav)', 'show')
+    openNav() {
+        console.log(this)
+        this.nav.classList.toggle(this.classToggle)
+    }
+
+    events() {
+       
+        this.btn.addEventListener('click', this.openNav.bind(this))
+    }
+}
+
+
+const mobileNav = new MobileNav(document.getElementById("hamburger-btn"), document.getElementById("header-nav"), 'show')
 
